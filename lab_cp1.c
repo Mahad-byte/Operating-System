ed16c4d896285474f85f1ffc11426af1c49d1447
Mahad-byte <113996965+Mahad-byte@users.noreply.github.com>
2023-11-25 23:39:25 +0500

    Update lab_cp1.c
    Proper synchronization of processes

--- lab_cp1.c
+++ lab_cp1.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
+
 #include<sys/wait.h>
+
 #include<stdlib.h>
 
 int main() {
   pid_t A, B, D, C, E, F;
+  int status;
   A = fork();
   if (A == 0) {
     printf("--C\n");
@@ -11,45 +14,44 @@ int main() {
     if (C == 0) {
       printf("---G\n");
       exit(0);
-    }
-    else if(C > 0){
-    wait(NULL);
+    } else if (C > 0) {
+      wait(NULL);
     }
   } else if (A > 0) {
     printf("A\n");
-    wait(NULL);		//waits for C
+    wait(NULL); //waits for C
+
     B = fork();
     if (B > 0) {
       printf("--B\n");
-      B = fork();	//B forks E
+      B = fork(); //B forks E
       if (B == 0) {
         printf("---E\n");
         exit(0);
-      } else if (B > 0) {
-        wait(NULL);	//wait for E
-        B = fork();	//B forks F
+      } else {
+        B = fork(); //B forks F
         if (B == 0) {
           printf("---F\n");
           exit(0);
-        }
-        else if(B > 0){
-        wait(NULL);	//wait for F
+        } else {
+          waitpid(B, & status, 0); //wait for F
+          B = fork();
+          if (B == 0) {
+            printf("---D\n");
+            D = fork(); //D forks H
+            if (D == 0) {
+              printf("----H\n");
+              exit(0);
+            } else {
+              waitpid(B, & status, 0); 
+            }
+          } else {
+            waitpid(D, & status, 0);
+          }
         }
       }
     }
-     else if (B == 0) {
-      printf("---D\n");
-      wait(NULL);	//wait for other children of B
-      usleep(3000000);
-      D = fork();	//D forks H
-      if (D == 0) {
-        printf("D forks\n----H\n");
-        exit(0);
-      }
-      else if(D > 0){
-      wait(NULL);	//wait for H so that H doesn't becomes orphan
-      }
-    }
+
   }
 
 }
